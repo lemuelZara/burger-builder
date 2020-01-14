df9d8cd9956011fd4fd4fc9be482dd72ad133e4e
lemuelZara <lemuel_zara@outlook.com>
2020-01-14 13:10:37 -0300

    change of Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Hoc from '../../hoc/Hoc'
 import styles from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const Layout = props => (
-    <Hoc>
-        <Toolbar />
-        <main className={styles.Content}>
-            {props.children}
-        </main>
-    </Hoc>
-)
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
 
+    sideDrawerClosedHandler = () => {
+        this.setState({
+            showSideDrawer: false
+        })
+    }
+
+    render() {
+        return (
+            <Hoc>
+                <Toolbar />
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+                <main className={styles.Content}>
+                    {this.props.children}
+                </main>
+            </Hoc>
+        )
+    }
+}
 export default Layout
