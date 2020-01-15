c48c870ebde52aece0e9fc2fade52f3a3212a81b
lemuelZara <lemuel_zara@outlook.com>
2020-01-15 09:26:00 -0300

    change of directory

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+import Hoc from '../../hoc/Hoc'
+import styles from './Layout.module.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
+
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({
+            showSideDrawer: false
+        })
+    }
+
+    sideDrawerToogleHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer }
+        })
+    }
+
+    render() {
+        return (
+            <Hoc>
+                <Toolbar drawerToogleClick={this.sideDrawerToogleHandler} />
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+                <main className={styles.Content}>
+                    {this.props.children}
+                </main>
+            </Hoc>
+        )
+    }
+}
+export default Layout
