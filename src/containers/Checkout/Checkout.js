d11f735b5e66c94c6e0b2ab771eed5332b02dca1
lemuelZara <lemuel_zara@outlook.com>
2020-01-17 15:45:16 -0300

    react router

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
 
 class Checkout extends Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            meat: 1,
+            cheese: 1,
+            bacon: 1
+        }
+    }
     render() {
         return (
-            <h1></h1>
+            <CheckoutSummary ingredients={this.state.ingredients} />
         )
     }
 }
