96e8dfd096906f9543133fa2e7b15ac69fc51145
lemuelZara <lemuel_zara@outlook.com>
2020-01-20 16:28:26 -0300

    add modifications

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,11 +2,19 @@ import React from 'react'
 
 import classes from './Order.module.css'
 
-const Order = props => (
-    <div className={classes.Order}>
-        <p>Ingredients:</p>
-        <p>Price: <strong></strong></p>
-    </div>
-)
+const Order = props => {
+    const ingredients = []
+
+    for (let ingredientName in props.ingredients) {
+        ingredients.push(props.ingredients[ingredientName])
+    }
+
+    return (
+        <div className={classes.Order}>
+            <p>Ingredients: {ingredients}</p>
+            <p>Price: <strong>{Number.parseFloat(props.price).toFixed(2)}</strong></p>
+        </div>
+    )
+}
 
 export default Order
