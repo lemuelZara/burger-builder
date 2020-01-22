01c78faa3e99774138abc5b306e357660821a6c1
lemuelZara <lemuel_zara@outlook.com>
2020-01-22 15:45:08 -0300

    add

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,37 +4,54 @@ import classes from './Input.module.css'
 
 const Input = props => {
     let inputElement = null
+    const inputClasses = [classes.InputElement]
+
+    if (props.invalid
+        && props.shouldValidate
+        && props.touched) inputClasses.push(classes.Invalid)
 
     switch (props.elementType) {
         case ('input'):
             inputElement = <input
-                className={classes.InputElement}
+                className={inputClasses.join(' ')}
                 {...props.elementConfig}
-                value={props.value} />
+                value={props.value}
+                onChange={props.changed} />
             break
         case ('select'):
-            inputElement = <select
-                className={classes.InputElement}
-                value={props.value}>
-                {props.elementConfig.options.map(op => (
-                    <option value={op.value}>
-                        {op.displayValue}
-                    </option>
-                ))}
-            </select>
+            inputElement = (
+                <select
+                    className={inputClasses.join(' ')}
+                    value={props.value}
+                    onChange={props.changed}>
+                    {props.elementConfig.options.map(op => (
+                        <option
+                            key={op.value}
+                            value={op.value}>
+                            {op.displayValue}
+                        </option>
+                    ))}
+                </select>
+            )
             break
         default:
             inputElement = <input
-                className={classes.InputElement}
+                className={inputClasses.join(' ')}
                 {...props.elementConfig}
                 value={props.value} />
             break
+    }
+
+    let validationError = null
+    if (props.invalid && props.touched) {
+        validationError = <p className={classes.ValidationError}>Please, enter a valid value!</p>
     }
 
     return (
         <div className={classes.Input}>
             <label className={classes.Label}>{props.label}</label>
             {inputElement}
+            {validationError}
         </div>
     )
 }
