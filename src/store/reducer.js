6b6a21b7db71246e9f559896201e50535ff74e85
lemuelZara <lemuel_zara@outlook.com>
2020-01-23 16:01:58 -0300

    adding logic to add ingredients through Redux

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,13 +1,44 @@
-import actionTypes from './actions'
+import * as actionTypes from './actions'
 
 const initialState = {
-    ingredients: null,
+    ingredients: {
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0
+    },
     totalPrice: 4,
 }
 
-const ingredientReducer = (prevState = initialState, action) => {
-    return {
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+};
 
+const ingredientReducer = (prevState = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.ADD_INGREDIENTS:
+            return {
+                ...prevState,
+                ingredients: {
+                    ...prevState.ingredients,
+                    [action.ingredientName]: prevState.ingredients[action.ingredientName] + 1
+                },
+                totalPrice: prevState.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+            }
+        case actionTypes.REMOVE_INGREDIENTS:
+            return {
+                ...prevState,
+                ingredients: {
+                    ...prevState.ingredients,
+                    [action.ingredientName]: prevState.ingredients[action.ingredientName] - 1
+                },
+                totalPrice: prevState.totalPrice - INGREDIENT_PRICES[action.ingredientName]
+            }
+        default:
+            return prevState
     }
 }
 
