dceff36928dafa63959b414d3223d2b9d894bc0f
lemuelZara <lemuel_zara@outlook.com>
2020-01-23 15:18:15 -0300

    add redux in project

---
+++ src/store/reducer.js
@@ -0,0 +1,14 @@
+import actionTypes from './actions'
+
+const initialState = {
+    ingredients: null,
+    totalPrice: 4,
+}
+
+const ingredientReducer = (prevState = initialState, action) => {
+    return {
+
+    }
+}
+
+export default ingredientReducer
