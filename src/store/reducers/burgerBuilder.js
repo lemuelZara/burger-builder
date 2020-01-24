97d4eb2f3d9d82ec831d97aedc06d7c5b5c55de1
lemuelZara <lemuel_zara@outlook.com>
2020-01-24 13:58:13 -0300

    ingredient order changed

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -36,7 +36,12 @@ const ingredientReducer = (prevState = initialState, action) => {
         case actionTypes.SET_INGREDIENTS:
             return {
                 ...prevState,
-                ingredients: action.ingredients,
+                ingredients: {
+                    salad: action.ingredients.salad,
+                    bacon: action.ingredients.bacon,
+                    cheese: action.ingredients.cheese,
+                    meat: action.ingredients.meat
+                },
                 error: false
             }
         case actionTypes.FETCH_INGREDIENTS_FAILED:
