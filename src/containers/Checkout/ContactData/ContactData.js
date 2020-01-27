a1936c327b50699f9ec89a885743ddb49b01e152
lemuelZara <lemuel_zara@outlook.com>
2020-01-27 09:06:15 -0300

    component ContactData in redux

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -6,6 +6,8 @@ import classes from './ContactData.module.css'
 import axios from '../../.././axios-orders'
 import Spinner from '../../../components/UI/Spinner/Spinner'
 import Input from '../../../components/UI/Input/Input'
+import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler'
+import * as actions from '../../../store/actions/index'
 
 class ContactData extends Component {
     state = {
@@ -97,8 +99,6 @@ class ContactData extends Component {
     orderHandler = event => {
         event.preventDefault()
 
-        this.setState({ loading: true });
-
         const formData = {}
         for (let inputIdentifier in this.state.orderForm) {
             formData[inputIdentifier] = this.state.orderForm[inputIdentifier].value
@@ -109,14 +109,8 @@ class ContactData extends Component {
             price: this.props.totalPrice,
             orderData: formData
         }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({ loading: false });
-                this.props.history.push('/')
-            })
-            .catch(error => {
-                this.setState({ loading: false });
-            })
+
+        this.props.onOrderBurger(order)
     }
 
     checkValidity = (value, rules) => {
@@ -196,4 +190,8 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps)(ContactData)
+const mapDispatchToProps = dispatch => {
+    onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData)) 
+}
+
+export default connect(mapStateToProps)(withErrorHandler(ContactData, axios))
---
+++ src/store/reducers/order.js
@@ -0,0 +1,9 @@
+import * as actionTypes from '../actions/actionTypes'
+
+const initialState = {
+
+}
+
+const reducer = (state = initialState, actions) => {
+
+}
