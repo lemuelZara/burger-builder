968f7a74734e1658392c15e2f387dbacf14dc782
lemuelZara <lemuel_zara@outlook.com>
2020-01-27 09:24:47 -0300

    work in order actions

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -191,7 +191,9 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = dispatch => {
-    onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData)) 
+    return {
+        onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
+    }
 }
 
-export default connect(mapStateToProps)(withErrorHandler(ContactData, axios))
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios))
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,9 +1,28 @@
 import * as actionTypes from '../actions/actionTypes'
 
 const initialState = {
-
+    orders: [],
+    loading: false
 }
 
 const reducer = (state = initialState, actions) => {
-
+    switch (actions.type) {
+        case actionTypes.PURCHASE_BURGER_SUCCESS:
+            const newOrder = {
+                ...actions.orderData,
+                id: actions.orderId
+            }
+            return {
+                ...state,
+                loading: false,
+                orders: state.orders.concat(newOrder)
+            }
+        case actionTypes.PURCHASE_BURGER_FAIL:
+            return {
+                ...state,
+                loading: false
+            }
+        default:
+            return state
+    }
 }
