a1a03dc1abac277c12f2ea68e615e1dca8ccd77c
lemuelZara <lemuel_zara@outlook.com>
2020-01-27 15:12:31 -0300

    fetch orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux'
 
 import Order from "../../components/Order/Order";
 import axios from '../../axios-orders'
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
+import * as actions from '../../store/actions/index'
 
 
 class Orders extends Component {
-    state = {
-        orders: [],
-        loading: true
+    componentDidMount() {
+        this.props.fetchOrders()
     }
 
     render() {
@@ -17,12 +18,18 @@ class Orders extends Component {
                 {this.state.orders.map(order => (
                     <Order
                         key={order.id}
-                        ingredients={order.ingredients} 
-                        price={order.price}/>
+                        ingredients={order.ingredients}
+                        price={order.price} />
                 ))}
             </div>
         )
     }
 }
 
-export default withErrorHandler(Orders, axios)
+const mapDispatchToProps = dispatch => {
+    return {
+        onFetchOrders: () => dispatch(actions.fetchOrders())
+    }
+}
+
+export default connect(null, mapDispatchToProps)(withErrorHandler(Orders, axios))
