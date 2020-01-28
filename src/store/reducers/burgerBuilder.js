360968d11b6c9c7481e1542d5a4269dfc0d0c2cc
lemuelZara <lemuel_zara@outlook.com>
2020-01-28 09:55:04 -0300

    continued modification of reducers

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,5 +1,6 @@
 import * as actionTypes from '../actions/actionTypes'
 import { updatedObject } from '../utility'
+import { setIngredients } from '../actions/burgerBuilder';
 
 const initialState = {
     ingredients: null,
@@ -14,57 +15,63 @@ const INGREDIENT_PRICES = {
     bacon: 0.7
 };
 
+const addIngredient = (prevState, action) => {
+    const updatedIngredient = {
+        [action.ingredientName]: prevState.ingredients[action.ingredientName] + 1
+    }
+
+    const updatedIngredients = updatedObject(
+        prevState.ingredients,
+        updatedIngredient
+    )
+    const updatedState = {
+        ingredients: updatedIngredients,
+        totalPrice: prevState.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+    }
+    return updatedObject(prevState, updatedState)
+}
+
+const removeIngredient = (prevState, action) => {
+    const updatedIngredient_2 = {
+        [action.ingredientName]: prevState.ingredients[action.ingredientName] - 1
+    }
+
+    const updatedIngredients_2 = updatedObject(
+        prevState.ingredients,
+        updatedIngredient_2
+    )
+
+    const updatedState_2 = {
+        ingredients: updatedIngredients_2,
+        totalPrice: prevState.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+    }
+    return updatedObject(prevState, updatedState_2)
+}
+
+const setIngredient = (prevState, action) => {
+    return updatedObject(
+        prevState,
+        {
+            ingredients: {
+                salad: action.ingredients.salad,
+                bacon: action.ingredients.bacon,
+                cheese: action.ingredients.cheese,
+                meat: action.ingredients.meat
+            },
+            error: false,
+            totalPrice: 4
+        }
+    )
+}
+
 const ingredientReducer = (prevState = initialState, action) => {
     switch (action.type) {
-        case actionTypes.ADD_INGREDIENTS:
-            const updatedIngredient = {
-                [action.ingredientName]: prevState.ingredients[action.ingredientName] + 1
-            }
-
-            const updatedIngredients = updatedObject(
-                prevState.ingredients,
-                updatedIngredient
-            )
-            const updatedState = {
-                ingredients: updatedIngredients,
-                totalPrice: prevState.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-            }
-            return updatedObject(prevState, updatedState)
-
-        case actionTypes.REMOVE_INGREDIENTS:
-            const updatedIngredient_2 = {
-                [action.ingredientName]: prevState.ingredients[action.ingredientName] - 1
-            }
-
-            const updatedIngredients_2 = updatedObject(
-                prevState.ingredients,
-                updatedIngredient_2
-            )
-
-            const updatedState_2 = {
-                ingredients: updatedIngredients_2,
-                totalPrice: prevState.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-            }
-            return updatedObject(prevState, updatedState_2)
-
-        case actionTypes.SET_INGREDIENTS:
-            return updatedObject(
-                prevState,
-                {
-                    ingredients: {
-                        salad: action.ingredients.salad,
-                        bacon: action.ingredients.bacon,
-                        cheese: action.ingredients.cheese,
-                        meat: action.ingredients.meat
-                    },
-                    error: false,
-                    totalPrice: 4
-                }
-            )
+        case actionTypes.ADD_INGREDIENTS: return addIngredient(prevState, action)
+        case actionTypes.REMOVE_INGREDIENTS: return removeIngredient(prevState, action)
+        case actionTypes.SET_INGREDIENTS: return setIngredient(prevState, action)
         case actionTypes.FETCH_INGREDIENTS_FAILED:
             return updatedObject(prevState, { error: true })
-        default:
-            return prevState
+        default: return prevState
     }
 }
 
