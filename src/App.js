bb911e8d694362e17df3aba4fbedab9dcb9768e7
lemuelZara <lemuel_zara@outlook.com>
2020-01-28 10:22:48 -0300

    authentication

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders'
+import Auth from './containers/Auth/Auth'
 
 class App extends Component {
-  render () {
+  render() {
     return (
       <div>
         <Layout>
           <Switch>
             <Route path="/checkout" component={Checkout} />
-            <Route path="/" exact component={BurgerBuilder} />
             <Route path="/orders" component={Orders} />
+            <Route path="/auth" component={Auth} />
+            <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
       </div>
