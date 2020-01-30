106a54abb0a39df329045948df4561bba24a8163
lemuelZara <lemuel_zara@outlook.com>
2020-01-30 15:56:46 -0300

    using updatedObject in app

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,5 +1,5 @@
 import * as actionType from '../actions/actionTypes'
-import { updatedObject } from '../utility'
+import { updatedObject } from '../../shared/utility'
 
 // Recebo os dados vindo do store/actions
 
