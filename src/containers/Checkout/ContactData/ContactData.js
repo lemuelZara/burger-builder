f6c58b7ab77fd0bcb327486612bd31fcdfa99441
lemuelZara <lemuel_zara@outlook.com>
2020-01-30 16:01:00 -0300

    shared validation method

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,7 +8,7 @@ import Spinner from '../../../components/UI/Spinner/Spinner'
 import Input from '../../../components/UI/Input/Input'
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler'
 import * as actions from '../../../store/actions/index'
-import { updatedObject } from '../../../shared/utility'
+import { updatedObject, checkValidity } from '../../../shared/utility'
 
 class ContactData extends Component {
     state = {
@@ -114,23 +114,13 @@ class ContactData extends Component {
         this.props.onOrderBurger(order, this.props.token)
     }
 
-    checkValidity = (value, rules) => {
-        let isValid = true
-
-        if (rules.required) isValid = value.trim() !== '' && isValid
-        if (rules.minLength) isValid = value.length >= rules.minLength && isValid
-        if (rules.maxLength) isValid = value.length <= rules.maxLength && isValid
-
-        return isValid
-    }
-
     inputChangedHandler = (event, inputIdentifier) => {
 
         const updatedFormElement = updatedObject(
             this.state.orderForm[inputIdentifier],
             {
                 value: event.target.value,
-                valid: this.checkValidity(
+                valid: checkValidity(
                     event.target.value,
                     this.state.orderForm[inputIdentifier].validation
                 ),
