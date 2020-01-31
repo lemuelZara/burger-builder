b0e99916ffbe1ef4336c296ece9c8d250345df4b
lemuelZara <lemuel_zara@outlook.com>
2020-01-30 23:06:06 -0300

    continuing testing

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -10,9 +10,24 @@ configure({
 })
 
 describe('<NavigationItems />', () => {
+    let wrapper
+
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />)
+    })
+
     it('should render two <NavigationItem /> elements if not authenticated',
+        () => { expect(wrapper.find(NavigationItem)).toHaveLength(2) })
+
+    it('should render three <NavigationItem /> elements if authenticated',
         () => {
-            const wrapper = shallow(<NavigationItems />)
-            expect(wrapper.find(NavigationItem)).toHaveLength(2)
+            wrapper.setProps({ isAuthenticated: true })
+            expect(wrapper.find(NavigationItem)).toHaveLength(3)
+        })
+
+    it('should an exact logout button',
+        () => {
+            wrapper.setProps({ isAuthenticated: true })
+            expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true)
         })
 })
