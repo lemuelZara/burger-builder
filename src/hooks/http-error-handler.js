d9898ee7667fcb8cc661d05d0066761fa908e7f0
lemuelZara <lemuel_zara@outlook.com>
2020-02-11 15:48:26 -0300

    -

--- src/hooks/http-error-handler.js
+++ src/hooks/http-error-handler.js
@@ -17,7 +17,7 @@ export default httpClient => {
             httpClient.interceptors.request.eject(reqInterceptor)
             httpClient.interceptors.response.eject(resInterceptor)
         }
-    }, [reqInterceptor, resInterceptor])
+    }, [httpClient, reqInterceptor, resInterceptor])
 
     const errorConfirmedHandler = () => {
         setError(null)
